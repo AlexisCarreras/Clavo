fbf8529671ea0859fafe94575b6d6889fe95a250
AlexisCarreras <alexiscarreras@hotmail.com>
2022-08-20 15:30:57 -0300

    chore: finished headboard component

--- src/components/SecondScreen/Headboard/index.tsx
+++ src/components/SecondScreen/Headboard/index.tsx
@@ -1,11 +1,31 @@
+import { makeStyles, Typography } from '@material-ui/core';
+import Logo from '../../../assets/Images/Logo/logo.png';
+
 import './index.css';
 
+const useStyles = makeStyles ({
+  text: {
+    fontFamily: 'Another America',
+    fontStyle: 'normal',
+    fontSize: '70px',
+    color: '#FAFAFA',
+  },
+}); 
+
 const Headboard = () => {
+  const classes = useStyles();
+
   return (
-    <>
-        Headboard
-    </>
+    <div className='background'>
+      <div className='container'> 
+        <img src={ Logo } className='logo' alt="Logo de Clavo" />
+        <Typography className={classes.text} variant="h1" component="h2">
+          C.L.A.V.O
+        </Typography>
+      </div>
+    </div>
   )
 }
 
 export default Headboard;
+ 
