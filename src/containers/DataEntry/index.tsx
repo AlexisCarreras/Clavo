266a835a07166a999e50694a7080f4f4bc08a617
Alexis Carreras <acarreras@cdainfo.com>
2022-09-08 22:47:37 -0300

    chore: fix prueba en produccion 7

--- src/containers/DataEntry/index.tsx
+++ src/containers/DataEntry/index.tsx
@@ -1,20 +1,10 @@
-import { makeStyles } from '@material-ui/core';
 import Modal from '../../components/Confirm/Modal';
 
-const useStyles = makeStyles ({
-    root: {
-      width: '100vw',
-    },
-});
-
 const DataEntry = () => {
-  const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Modal /> 
-    </div> 
+      <Modal />
   )
 }
- 
+
 export default DataEntry;
