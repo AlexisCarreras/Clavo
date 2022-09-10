40175ecca177f2b33ae61c26c43126cf51403d90
Alexis Carreras <acarreras@cdainfo.com>
2022-09-10 01:56:39 -0300

    chore: responsive en pantalla de obtenerentradas

--- src/containers/TicketOperation/index.tsx
+++ src/containers/TicketOperation/index.tsx
@@ -1,20 +1,38 @@
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { EventInformation, Headboard, Header, Overview } from '../../components';
-import './index.css';
 
+const useStyles = makeStyles ({
+  root: {
+    width: '100%',
+    backgroundColor: '#F6F6F6',
+  },
+  item: {
+    display: 'flex',
+    justifyContent: 'center',
+    margin: '6vh 0 6vh 0',
+  },
+});   
+   
 const TicketOperation = () => {
+  const classes = useStyles();
 
   localStorage.setItem('pageIndex', 'true');
-
+ 
   return (
     <>
       <Header />
       <Headboard />
-      <div className='containerCards'>
-        <Overview />
-        <EventInformation />
-      </div>
+      <Grid container className={classes.root}>
+          <Grid className={classes.item} item xs={12} md={6}>   
+            <Overview /> 
+          </Grid>
+          <Grid className={classes.item} item xs={12} md={6}>
+            <EventInformation />
+          </Grid>
+      </Grid>
     </>
   )
-}
+} 
 
 export default TicketOperation;
